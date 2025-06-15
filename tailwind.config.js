78d9834cfa5201d04f24be50cb7d516a56c73c6c
mjodims <jhodiemarties@gmail.com>
2025-06-15 21:51:00 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+        /** @type {import('tailwindcss').Config} */
+        module.exports = {
+          content: [
+            "./src/**/*.{js,jsx,ts,tsx}", // Pastikan ini mencakup semua file React Anda
+          ],
+          theme: {
+            extend: {},
+          },
+          plugins: [],
+        }
+        
